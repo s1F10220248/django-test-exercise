3c97507c005bf23753fa7cd193bbd73cb8ab04c9
s1F10220248 <s1f102202482@iniad.org>
2023-07-06 12:31:59 +0900

    詳細ページのテストを修正

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -33,4 +32,3 @@ def detail(request, task_id):
             'task': task,
     }
     return render(request, 'todo/detail.html', context)
-    
